3154d66e9c1d307ee023c4275b6704f110a67fb8
remojansen <remo.jansen@wolksoftware.com>
2016-06-26 00:57:41 +0100

    Updated dependencies

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,5 @@
 import thunk from "redux-thunk";
-import bootstrap from "redux-bootstrap";
+import { bootstrap } from "redux-bootstrap";
 import routes from "./config/routes";
 import appReducer from "./reducers/app_reducer";
 import logReducer from "./reducers/log_reducer";
